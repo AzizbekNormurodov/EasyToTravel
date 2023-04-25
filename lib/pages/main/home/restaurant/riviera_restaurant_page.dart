6e8aa6ef1180125038514fb81310c965ef391894
Azizbeknormurodov <09991azizbek@gmail.com>
2023-04-25 14:27:51 +0500

    second commit

--- lib/pages/main/home/restaurant/riviera_restaurant_page.dart
+++ lib/pages/main/home/restaurant/riviera_restaurant_page.dart
@@ -1,5 +1,6 @@
 import 'package:easy_to_travel/core/theme/theme_colors.dart';
 import 'package:easy_to_travel/core/theme/theme_text_styles.dart';
+import 'package:easy_to_travel/pages/main/home/restaurant/widget/book_widget.dart';
 import 'package:easy_to_travel/pages/main/home/restaurant/widget/sliver_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -180,88 +181,7 @@ class RivieraRestaurantPage extends StatelessWidget {
               ),
             ),
           ),
-          SliverToBoxAdapter(
-            child: Container(
-              width: 375,
-              height: 328,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
-                color: Colors.white,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Book a visit",
-                      style: ThemeTextStyles.status,
-                    ),
-                    SilverWidget(),
-                    Text("Duration of stay"),
-                    Container(
-                      width: double.infinity,
-                      height: 48,
-                      decoration: BoxDecoration(
-                        color: Color(0xffF5F5F5),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(12),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 12, right: 12),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Usage - 1 hour"),
-                            Icon(Icons.keyboard_arrow_down),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 12, bottom: 12),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            Color.fromRGBO(0, 130, 255, 0.1),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          "Book from \$80",
-                          style: TextStyle(color: ThemeColors.primary),
-                        ),
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Color.fromRGBO(0, 130, 255, 0.1),
-                        ),
-                      ),
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset("assets/png/img.png",
-                              height: 23,
-                              width: 23,
-                              color: ThemeColors.primary),
-                          Text(
-                            "Do you want to buy fast track",
-                            style: TextStyle(color: ThemeColors.primary),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          BookWidget(),
         ],
       ),
     );
